312e9f5960e2b2516e6203bd2ef9dad0303b2997
henriqueNogueiraa <150245983+HenriquePiZZA@users.noreply.github.com>
2024-10-08 10:57:13 -0300

    :memo: configurei corretamente as rotas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,15 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { ListagemDeAtoresComponent } from './components/listagem-de-atores/listagem-de-atores.component';
 import { AtorComponent } from './components/ator/ator.component';
+import { ClasseComponent } from './components/classe/classe.component';
+import { DiretorComponent } from './components/diretor/diretor.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'ator', pathMatch: 'full' }, // Rota padrão
   { path: 'ator', component: ListagemDeAtoresComponent }, // Rota do ator
-  { path: 'cadastroAtor', component: AtorComponent } // Rota do ator
-
+  { path: 'ator/cadastroAtor', component: AtorComponent }, // Rota do ator
+  { path: 'classe', component: ClasseComponent },
+  { path: 'diretor', component: DiretorComponent }
 ];
 
 export const appConfig: ApplicationConfig = {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,28 @@
 import { Routes } from '@angular/router';
 import { AtorComponent } from './components/ator/ator.component';
 import { ListagemDeAtoresComponent } from './components/listagem-de-atores/listagem-de-atores.component';
+import { ClasseComponent } from './components/classe/classe.component';
+import { DiretorComponent } from './components/diretor/diretor.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: AtorComponent
-    }, {
-        path: 'new',
-        component: AtorComponent
-    },
+    }, 
     {
-        path: 'cadastroAtor/:id',
+        path: 'ator/cadastroAtor',
         component: AtorComponent
     },
     {
         path: 'ator',
         component: ListagemDeAtoresComponent
+    },
+    {
+        path: 'classe',
+        component: ClasseComponent
+    },
+    {
+        path: 'diretor',
+        component: DiretorComponent
     }
 ];
