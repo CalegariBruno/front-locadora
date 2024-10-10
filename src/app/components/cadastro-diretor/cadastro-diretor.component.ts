d06d0f1443e659d657a6f30778df8096f396aa62
henriqueNogueiraa <150245983+HenriquePiZZA@users.noreply.github.com>
2024-10-10 16:21:32 -0300

    :memo: Add tela de cadastro de diretor

---
+++ src/app/components/cadastro-diretor/cadastro-diretor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CadastroTemplateComponent } from '../cadastro-template/cadastro-template.component';
+@Component({
+  selector: 'app-cadastro-diretor',
+  standalone: true,
+  imports: [CadastroTemplateComponent],
+  templateUrl: './cadastro-diretor.component.html',
+  styleUrl: './cadastro-diretor.component.css'
+})
+export class CadastroDiretorComponent {
+
+}
