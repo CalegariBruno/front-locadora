0cd6791d9afabf5121bc32f5bd6decb6ed6f9bc7
bruno <brunocalegari7@gmail.com>
2024-10-10 19:54:55 -0300

    feat: add integração com api (listagem-diretor)

---
+++ src/app/components/listagem-diretor/listagem-diretor.component.ts
@@ -0,0 +1,57 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { RouterLink } from '@angular/router';
+import { Diretor } from '../../models/diretor/diretor';
+import { DiretorService } from '../../services/diretor/diretor.service';
+
+@Component({
+  selector: 'app-diretor',
+  standalone: true,
+  imports: [
+    CommonModule, 
+    MatTableModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatCardModule,
+    RouterLink
+  ],
+  templateUrl: './listagem-diretor.component.html',
+  styleUrl: './listagem-diretor.component.css'
+})
+export class DiretorComponent implements OnInit{
+
+  diretores: Diretor[] = [];
+  displayedColumns: string[] = ['nome', 'acoes'];
+  @Input() tipo: string = 'Diretores';
+
+  constructor(private diretorService: DiretorService) { }
+
+  ngOnInit(): void {
+    this.exibirDiretores();
+  }
+
+  exibirDiretores(): void {
+    this.diretorService.listarDiretores().subscribe(
+      (data: Diretor[]) => {
+        this.diretores = data;
+        console.log(this.diretores); // lembrar de tirar
+      },
+      (error) => {
+        console.error('Erro ao carregar a lista de atores', error);
+      }      
+    );
+  }
+
+  editarDiretor(diretor: any) {
+    // Configurar para abrir a tela de edição/cadastro
+  }
+
+  excluirDiretor(diretor: any) {
+    // Implementar a lógica para excluir o Diretor da lista
+    this.diretores = this.diretores.filter(c => c.id !== diretor.id);   
+  }
+}
