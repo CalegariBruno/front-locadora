da9bbe0171b6eb880fca626d7888a394a1283e17
henriqueNogueiraa <150245983+HenriquePiZZA@users.noreply.github.com>
2024-10-10 16:20:41 -0300

    :memo: Ajustado tela de casdastro de ator

---
+++ src/app/components/cadastro-ator/cadastro-ator.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CadastroTemplateComponent } from '../cadastro-template/cadastro-template.component';
+@Component({
+  selector: 'app-cadastro-ator',
+  standalone: true,
+  imports: [
+    CadastroTemplateComponent
+  ],
+  templateUrl: './cadastro-ator.component.html',
+  styleUrl: './cadastro-ator.component.css'
+})
+export class CadastroAtorComponent {
+
+}
