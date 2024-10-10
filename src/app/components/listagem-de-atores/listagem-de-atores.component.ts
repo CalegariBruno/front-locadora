3f981736ea2f206ab4a5dd01811d67a92085f8ee
bruno <brunocalegari7@gmail.com>
2024-10-10 19:54:27 -0300

    feat: add integracao com a api (listagem-ator)

--- src/app/components/listagem-de-atores/listagem-de-atores.component.ts
+++ src/app/components/listagem-de-atores/listagem-de-atores.component.ts
@@ -1,35 +1,50 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
+import { AtorService } from '../../services/ator/ator.service';
+import { Ator } from '../../models/ator/ator';
 
 @Component({
   selector: 'app-listagem-de-atores',
   standalone: true,
-  imports: [
-    CommonModule, 
-    MatTableModule, 
-    MatButtonModule, 
-    MatIconModule, 
+  imports: [    
+    CommonModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
     MatCardModule,
     RouterLink],
   templateUrl: './listagem-de-atores.component.html',
   styleUrl: './listagem-de-atores.component.css'
 })
-export class ListagemDeAtoresComponent {
-  // Lista de atores com dados de exemplo
-  atores = [
-    { id: 1, nome: 'Ator 1' },
-    { id: 2, nome: 'Ator 2' },
-    { id: 3, nome: 'Ator 3' }
-  ];
 
+export class ListagemDeAtoresComponent implements OnInit {
+
+  atores: Ator[] = [];
+  displayedColumns: string[] = ['nome', 'acoes'];
   @Input() tipo: string = 'Atores';
 
-  displayedColumns: string[] = ['nome', 'acoes'];
+  constructor(private atorService: AtorService) { }
+
+  ngOnInit(): void {
+    this.exibirAtores();
+  }
+
+  exibirAtores(): void {
+    this.atorService.listarAtores().subscribe(
+      (data: Ator[]) => {
+        this.atores = data;
+        console.log(this.atores); // lembrar de tirar
+      },
+      (error) => {
+        console.error('Erro ao carregar a lista de atores', error);
+      }      
+    );
+  }
 
   editarAtor(ator: any) {
     // Configurar para abrir a tela de edição/cadastro
@@ -37,7 +52,6 @@ export class ListagemDeAtoresComponent {
 
   excluirAtor(ator: any) {
     // Implementar a lógica para excluir o ator da lista 
-    this.atores = this.atores.filter(c => c.id !== ator.id);   
-  
+    //this.atores = this.atores.filter(c => c.id !== ator.id);
   }
 }
