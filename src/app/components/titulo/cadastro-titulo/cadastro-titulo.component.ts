86555f65472f79a72a349723da8b7247a5f141c2
bruno <brunocalegari7@gmail.com>
2024-10-22 16:13:32 -0300

    feat: estrutura da page titulo

---
+++ src/app/components/titulo/cadastro-titulo/cadastro-titulo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-titulo',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-titulo.component.html',
+  styleUrl: './cadastro-titulo.component.css'
+})
+export class CadastroTituloComponent {
+
+}
---
+++ src/app/components/titulo/listagem-titulo/listagem-titulo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listagem-titulo',
+  standalone: true,
+  imports: [],
+  templateUrl: './listagem-titulo.component.html',
+  styleUrl: './listagem-titulo.component.css'
+})
+export class ListagemTituloComponent {
+
+}
