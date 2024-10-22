bce09ece1f99931f7d46f6b25cba6414f699ec23
bruno <brunocalegari7@gmail.com>
2024-10-22 16:09:28 -0300

    feat: estrutura da page de item

---
+++ src/app/components/item/cadastro-item/cadastro-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-item.component.html',
+  styleUrl: './cadastro-item.component.css'
+})
+export class CadastroItemComponent {
+
+}
---
+++ src/app/components/item/listagem-item/listagem-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listagem-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './listagem-item.component.html',
+  styleUrl: './listagem-item.component.css'
+})
+export class ListagemItemComponent {
+
+}
